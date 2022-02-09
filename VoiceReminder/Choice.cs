d0e30619993068449c775176da85a3893987515b
YokaEnenra <lartaman1337@gmail.com>
2022-02-09 17:52:09 +0200

    Choose class renaming Change to comply with class naming conventions

---
+++ VoiceReminder/Choice.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceReminder
+{
+    internal static class Choice
+    {
+        internal static bool Language()
+        {
+            LanguageChooseWindow languageForm = new LanguageChooseWindow();
+            languageForm.ShowDialog();
+            if (languageForm.language != "none")
+            {
+                var preferredLanguage = languageForm.language;
+                ProgramRegistry.SetLanguage(preferredLanguage);
+                return true;
+            }
+            return false;
+        }
+    }
+}
