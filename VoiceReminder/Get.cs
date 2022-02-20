82d8388179da62c46ee4ae1a5e91790f36a34a99
YokaEnenra <lartaman1337@gmail.com>
2022-02-20 21:54:40 +0200

    Added Get function Small function to solve the problem of repeating code in two classes

---
+++ VoiceReminder/Get.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VoiceReminder
+{
+    internal static class Get
+    {
+        internal static string Value(TextBox smthCount)
+        {
+            string someValue;
+            try
+            {
+                someValue = Convert.ToInt32(smthCount.Text).ToString();
+            }
+            catch (FormatException)
+            {
+                someValue = "0";
+            }
+            return someValue;
+        }
+    }
+}
