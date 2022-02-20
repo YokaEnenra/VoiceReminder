640e7eeeddbd9475e17ec9edc5b7f56c58d86ea8
YokaEnenra <lartaman1337@gmail.com>
2022-02-20 22:05:43 +0200

    Added Final code Final code to made program fully functional

--- VoiceReminder/Program.cs
+++ VoiceReminder/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static VoiceReminder.ProgramRegistry;
 
 namespace VoiceReminder
 {
@@ -11,12 +12,58 @@ namespace VoiceReminder
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
-        [STAThread]
+        [STAThread] 
+        
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            try
+            {
+                OpenRegistry();
+                if (!ProgramExistence() || !GetDataPath())
+                {
+                    throw new ProgramNotExistException();
+                }
+            }
+            catch (Exception)
+            {
+                FirstRunActions();
+                Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\.VoiceReminder\Audio");
+                Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\.VoiceReminder\TextData");
+
+            }
+            if (GetLanguage() == "none" || GetLanguage() == null)
+            {
+                if (!Choice.Language())
+                {
+                    return;
+                }
+                
+            }
+            
+            ProgramDictionary.Set();
+            CloseRegistry();
+
+            bool onlyInstance;
+
+            Mutex mtx = new Mutex(true, "VoiceReminder", out onlyInstance); // используйте имя вашего приложения
+
+            // Если другие процессы не владеют мьютексом, то
+            // приложение запущено в единственном экземпляре
+            if (onlyInstance)
+            {
+                Start = DateTime.Now;
+                Task.Run(ReminderTracker.StartTracking);
+                Application.Run(new MainWindow());
+            }
+            else
+            {
+                MessageBox.Show(
+                    ProgramDictionary.Words["applicationWorking"],
+                   ProgramDictionary.Words["info"],
+                   MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
         }
     }
 }
---
+++ VoiceReminder/ProgramRegistry.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows.Forms;
+using AccessViolationException = System.AccessViolationException;
+
+namespace VoiceReminder
+{
+    internal static class ProgramRegistry
+    {
+        private static RegistryKey _currentUserKey;
+        private static RegistryKey _softwareMachineKey;
+        private static RegistryKey _yokaIncKey;
+        internal static string programDataPath;
+        //TODO Удалить хуйню
+        internal static DateTime Start;
+        internal static RegistryKey VoiceReminderKey { get; set; }
+
+        internal static void OpenRegistry()
+        {
+            _currentUserKey = Registry.CurrentUser;
+            _softwareMachineKey = _currentUserKey.OpenSubKey("SOFTWARE", true);
+            _yokaIncKey = _softwareMachineKey.OpenSubKey("YokaInc", true);
+            VoiceReminderKey = _yokaIncKey.OpenSubKey("VoiceReminder", true);
+        }
+
+        internal static void CloseRegistry()
+        {
+            VoiceReminderKey.Close();
+            _yokaIncKey.Close();
+            _softwareMachineKey.Close();
+            _currentUserKey.Close();
+        }
+
+        internal static bool ProgramExistence()
+        {
+            if ((string)VoiceReminderKey.GetValue("program_existance") != "true" || VoiceReminderKey.GetValue("program_existance") == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        internal static void FirstRunActions()
+        {
+            _yokaIncKey = _softwareMachineKey.CreateSubKey("YokaInc");
+            VoiceReminderKey = _yokaIncKey.CreateSubKey("VoiceReminder");
+            VoiceReminderKey.SetValue("program_existance", "true");
+            Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run\", "VoiceReminder",
+                "\"" + Application.ExecutablePath + "\"" + " /MINIMIZED");
+            VoiceReminderKey.SetValue("program_data_path", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.VoiceReminder"); 
+        }
+
+        internal static object GetLanguage()
+        {
+            OpenRegistry();
+            var language_t = (string)VoiceReminderKey.GetValue("language");
+            CloseRegistry();
+            return language_t;
+        }
+
+        internal static void SetLanguage(string preferredLanguage)
+        {
+            OpenRegistry();
+            VoiceReminderKey.SetValue("language", preferredLanguage);
+            CloseRegistry();
+        }
+
+        internal static bool GetDataPath()
+        {
+            try
+            {
+                OpenRegistry();
+                programDataPath = (string) VoiceReminderKey.GetValue("program_data_path");
+                CloseRegistry();
+                return true;
+            }
+            catch (AccessViolationException)
+            {
+                return false;
+            }
+           
+        }
+    }
+}
