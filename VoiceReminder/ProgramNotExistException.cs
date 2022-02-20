3b4adae34d2ebd567ecde75c07a2729c0dbedf1b
YokaEnenra <lartaman1337@gmail.com>
2022-02-20 21:36:04 +0200

    Made custom exception Serves to find the first run of program

---
+++ VoiceReminder/ProgramNotExistException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceReminder
+{
+    internal class ProgramNotExistException : Exception
+    {
+        public ProgramNotExistException()
+        {
+            var msg = "Registry is bull or false";
+        }
+    }
+}
